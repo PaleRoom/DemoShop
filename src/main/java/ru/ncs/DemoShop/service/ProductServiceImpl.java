7ad778f470aee09129b3677154ee54d35f1624b6
PaleRoom <PushistiiYashikWr@Yandex.ru>
2022-11-22 16:43:24 +0400

    Added H2 database support

--- src/main/java/ru/ncs/DemoShop/service/ProductServiceImpl.java
+++ src/main/java/ru/ncs/DemoShop/service/ProductServiceImpl.java
@@ -10,7 +10,6 @@ import ru.ncs.DemoShop.exception.ProductNotFoundException;
 import ru.ncs.DemoShop.exception.ProductNotUpdatedException;
 import ru.ncs.DemoShop.model.Product;
 import ru.ncs.DemoShop.repository.ProductRepository;
-import ru.ncs.DemoShop.service.aop.LogExecutionTime;
 import ru.ncs.DemoShop.service.data.ProductDTO;
 import ru.ncs.DemoShop.service.immutable.ImmutableCreateProductRequest;
 import ru.ncs.DemoShop.service.immutable.ImmutableUpdateProductRequest;
@@ -30,7 +29,6 @@ public class ProductServiceImpl implements ProductService {
     private final ConversionService conversionService;
 
     @Override
-    @LogExecutionTime
     public List<ProductDTO> findAll() {
         List<Product> list = productRepository.findAll();
         List<ProductDTO> listDTO = new ArrayList<>();
@@ -95,12 +93,4 @@ public class ProductServiceImpl implements ProductService {
         productRepository.deleteById(id);
     }
 
-    @Transactional
-    public void increasePrice(double mod) {
-        List<Product> sourceList = productRepository.findAll();
-        for (Product product : sourceList) {
-            product.setPrice(product.getPrice() * mod);
-        }
-        productRepository.saveAll(sourceList);
-    }
 }
