49bbb43d4312a7560bf10f9d41b019f00e7cd4b3
PaleRoom <PushistiiYashikWr@Yandex.ru>
2022-11-25 15:55:11 +0400

    Some cosmetic changes

--- src/main/java/ru/ncs/DemoShop/controller/response/GetProductAdvice.java
+++ src/main/java/ru/ncs/DemoShop/controller/response/GetProductAdvice.java
@@ -17,38 +17,30 @@ public class GetProductAdvice implements ResponseBodyAdvice<GetProductResponse> 
     @Override
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
         String className = returnType.getContainingClass().toString();
-
         String methodName = returnType.getMethod().toString();
-
-        if (className.contains("ProductControllerImpl") && methodName.contains("getOneProduct")) {
-
-            return true;
-        }
-
-        return false;
+        return className.contains("ProductControllerImpl") && methodName.contains("getOneProduct");
     }
 
     @Override
     public GetProductResponse beforeBodyWrite(GetProductResponse body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
         ObjectMapper objectMapper = new ObjectMapper();
-        File file = new File ("src/main/resources/ExchangeRate.json");
-       double rate;
+        File file = new File("src/main/resources/ExchangeRate.json");
+        double rate;
         try {
-            ExchangeRate exchangeRate = objectMapper.readValue(file,ExchangeRate.class);
+            ExchangeRate exchangeRate = objectMapper.readValue(file, ExchangeRate.class);
             rate = exchangeRate.getExchangeRate();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
         return GetProductResponse.builder()
-                .name(body.getName()+": Тест REST Advice controller - ну это просто волшебство!")
+                .name(body.getName())
                 .availability(body.isAvailability())
                 .description(body.getDescription())
                 .amount(body.getAmount())
                 .amountUpdatedAt(body.getAmountUpdatedAt())
                 .category(body.getCategory())
                 .id(body.getId())
-                .price(body.getPrice()*rate)
+                .price(body.getPrice() * rate)
                 .build();
     }
 }
