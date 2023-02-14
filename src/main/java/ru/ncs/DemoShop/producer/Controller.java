47018449494a913b54592b6893280dc89772d8ef
Nicolas <98647243+PaleRoom@users.noreply.github.com>
2023-02-14 13:12:28 +0400

    - updated for kafka messaging

---
+++ src/main/java/ru/ncs/DemoShop/producer/Controller.java
@@ -0,0 +1,22 @@
+package ru.ncs.DemoShop.producer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class Controller {
+    private final Producer producer;
+
+    @PostMapping("/string")
+    public void sendString(@RequestParam String topic, @RequestParam int keyFrom, @RequestParam int keyTo, @RequestParam String data, @RequestParam int count) {
+        producer.sendString(topic, keyFrom, keyTo, data, count);
+    }
+
+    @PostMapping("/message")
+    public void sendMessage(@RequestParam String topic, @RequestParam int keyFrom, @RequestParam int keyTo, @RequestParam String data, @RequestParam int count) {
+        producer.sendMessage(topic, keyFrom, keyTo, data, count);
+    }
+}
